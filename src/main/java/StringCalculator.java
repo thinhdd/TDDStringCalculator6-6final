ae30cd08b4e4bb84d6d0c0d81ccadb3e95cd9c1f
mr.thinhdaoduc <mr.thinhdaoduc@gmail.com>
2013-06-06 23:20:36 +0700

    Finalcomit

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,66 @@
+import java.util.regex.Pattern;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: RuaTre_IT
+ * Date: 6/6/13
+ * Time: 10:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculator {
+    public int add(String s) {
+        if(s.isEmpty())
+            return 0;
+        if(s.contains("//") && s.contains("\n"))
+        {
+            if(s.indexOf("\n")-s.indexOf("//")>3)
+            {
+                String arc = s.substring(s.indexOf("\n")+1,s.length());
+                String cur[] = arc.split(Pattern.quote(s.substring(3,6)));
+                return Sum(cur);
+            }
+            else
+            {
+                String arc = s.substring(4,s.length());
+                String cur[] = arc.split(String.valueOf(s.charAt(2)));
+                return Sum(cur);
+            }
+        }
+        if(s.contains(",\n") || s.contains("\n,"))
+            return -1;
+        if(s.contains(",") && s.contains("\n"))
+        {
+            String cur[] = s.split("[,\n]");
+            return Sum(cur);
+        }
+        if(s.contains(","))
+        {
+            String cur[] = s.split(",");
+            return Sum(cur);
+        }
+        return Integer.parseInt(s);  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private int Sum(String[] cur) {
+        int sum=0;
+        String list="";
+        for(int i=0; i<cur.length; i++)
+        {
+            if(!cur[i].isEmpty())
+            {
+                if(Integer.parseInt(cur[i])>0)
+                {
+                    if(Integer.parseInt(cur[i])<1000)
+                    {
+                        sum=sum+Integer.parseInt(cur[i]);
+                    }
+                }
+                else
+                    list=list+cur[i]+" ";
+            }
+        }
+        if(list.length()>0)
+            throw new IllegalArgumentException(list);
+        return sum;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,90 @@
+import junit.framework.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: RuaTre_IT
+ * Date: 6/6/13
+ * Time: 10:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculatorTest {
+    @Test
+    public void testEmpty()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add(""),0);
+    }
+    @Test
+    public void testOneNumber()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("1"),1);
+    }
+    @Test
+    public void testOneDeli()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("1,2"),3);
+    }
+    @Test
+    public void testTwoDeli()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("1,2\n3"),6);
+    }
+    @Test
+    public void testTwoErrorDeli()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("1,\n2"),-1);
+    }
+
+    @Test
+    public void testSpecOneDeli()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("//;\n1;2;3"), 6);
+    }
+    @Rule
+    public ExpectedException ex= ExpectedException.none();
+    @Test
+    public void testNeganumber()
+    {
+        StringCalculator sc = new StringCalculator();
+        ex.expectMessage("-1 -2 ");
+        Assert.assertEquals(sc.add("-1,-2"),-1);
+
+    }
+    @Test
+    public void testBigNumber()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("1000,3"), 3);
+    }
+    @Test
+    public void testOneStringDeli()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("//[***]\n1***2"), 3);
+    }
+    @Test
+    public void testTwoStringDeli()
+    {
+        StringCalculator sc = new StringCalculator();
+        Assert.assertEquals(sc.add("//[***][,,]\n1***2,3"), 6);
+    }
+
+}
+class Check{
+    public static void main(String[] args) {
+        String s= "1***2,,4";
+        String cur[] = s.split("["+Pattern.quote("***")+"+"+Pattern.quote(",,")+"]");
+        for(String i : cur)
+            System.out.println(i);
+    }
+}
